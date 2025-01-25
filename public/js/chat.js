3cfcac9040b949eb2e9ad83b498d74e8653cfba2
Don <sleepyparadox@gmail.com>
2025-01-25 16:38:43 +1100

    working MessageFactory

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,75 +1,152 @@
 const chatContainer = document.getElementById('chatContainer');
 
-function appendMessage(message, sender) {
-    const messageElement = document.createElement('div');
-    messageElement.classList.add('chat-message', sender);
-    messageElement.textContent = message;
-    chatContainer.appendChild(messageElement);
-    chatContainer.scrollTop = chatContainer.scrollHeight;
-}
+class MessageFactory {
+    constructor(messagesBody) {
+        this.messagesBody = messagesBody;
+    }
 
-function appendImage(imgSrc, sender) {
-    const messageElement = document.createElement('img');
-    messageElement.classList.add('chat-message', sender);
-    messageElement.src = imgSrc;
-    chatContainer.appendChild(messageElement);
-    chatContainer.scrollTop = chatContainer.scrollHeight;
-}
+    clear() {
+        while (this.messagesBody.firstChild) {
+            this.messagesBody.removeChild(this.messagesBody.firstChild);
+        }
+    }
 
-function sendMessage() {
-    const userInput = document.getElementById('send-input');
-    const message = userInput.value.trim();
+    appendTimestamp(text) {
+        var div = document.createElement("div");
+        div.classList.add("text-center")
+        var innerText = document.createElement("p");
+        innerText.classList.add("small", "mb-1", "text-muted")
+        innerText.textContent = text;
+        
+        this.messagesBody.appendChild(div);
+        div.appendChild(innerText);
+    }
 
-    if (message) {
-        appendMessage(message, 'user');
-        userInput.value = '';
-        setTimeout(() => appendImage('./assets/thumbs-up.png', 'chatbot'), 1000);
+    appendInputMessage(text) {
+        const inputDiv = document.createElement("div");
+        inputDiv.classList.add("d-flex", "flex-row", "justify-content-end", "mb-4", "pt-1")
+        const inputInnerText = document.createElement("p");
+        inputInnerText.classList.add("small", "p-2", "me-3", "mb-1", "rounded-3", "bg-body-tertiary")
+        inputInnerText.textContent = text;
+        
+        this.messagesBody.appendChild(inputDiv);
+        inputDiv.appendChild(inputInnerText);
+    }
+
+    appendResponseMessage(text) {
+        const dad = this.newDadName();
+        this.messagesBody.appendChild(dad);
+
+        const inputDiv = document.createElement("div");
+        inputDiv.classList.add("d-flex", "flex-row", "justify-content-start")
+        
+        const dadImg = this.newDadImage();
+
+        const inputInnerText = document.createElement("p");
+        inputInnerText.classList.add("small", "p-2", "ms-3", "me-3", "mb-1", "rounded-3", "bg-body-tertiary")
+        inputInnerText.textContent = text;
+        
+        this.messagesBody.appendChild(inputDiv);
+        inputDiv.appendChild(dadImg);
+        inputDiv.appendChild(inputInnerText);
+    }
+
+    appendThumbSmall() {
+        const dad = this.newDadName();
+        this.messagesBody.appendChild(dad);
+
+        const inputDiv = document.createElement("div");
+        inputDiv.classList.add("d-flex", "flex-row", "justify-content-start")
+        
+        const dadImg = this.newDadImage();
+
+        const inputInnerText = document.createElement("p");
+        inputInnerText.classList.add("small", "p-2", "pt-0", "ms-3", "me-3", "mb-1", "rounded-3")
+        
+        const thumbImg = document.createElement("img");
+        thumbImg.src = "assets/thumbs-up.png";
+        thumbImg.classList.add("thumbs-up-small");
+
+        this.messagesBody.appendChild(inputDiv);
+        inputDiv.appendChild(dadImg);
+        inputDiv.appendChild(inputInnerText);
+        inputInnerText.appendChild(thumbImg);
+    }
+    appendThumbLarge() {
+        const dad = this.newDadName();
+        this.messagesBody.appendChild(dad);
+
+        const inputDiv = document.createElement("div");
+        inputDiv.classList.add("d-flex", "flex-row", "justify-content-start")
+        
+        const dadImg = this.newDadImage();
+
+        const inputInnerText = document.createElement("p");
+        inputInnerText.classList.add("small", "p-2", "pt-0", "ms-3", "me-3", "mb-1", "rounded-3")
+        
+        const thumbImg = document.createElement("img");
+        thumbImg.src = "assets/thumbs-up.png";
+        thumbImg.classList.add("thumbs-up-big");
+
+        this.messagesBody.appendChild(inputDiv);
+        inputDiv.appendChild(dadImg);
+        inputDiv.appendChild(inputInnerText);
+        inputInnerText.appendChild(thumbImg);
+    }
+    appendTyping() {
+        const dad = this.newDadName();
+        this.messagesBody.appendChild(dad);
+
+        const typingDiv = document.createElement("div");
+        typingDiv.classList.add("d-flex", "flex-row", "justify-content-start")
+
+        const dadImg = this.newDadImage();
+
+        const typingInner = document.createElement("p");
+        typingInner.classList.add("small", "p-2", "ms-3", "mb-3", "rounded-3", "bg-body-tertiary")
+        
+        const dotsDiv = document.createElement("div");
+        dotsDiv.classList.add("dots");
+
+        const dot1 = document.createElement("div");
+        dot1.classList.add("dot", "dot-1", "animate__animated", "animate__bounce");
+
+        const dot2 = document.createElement("div");
+        dot2.classList.add("dot", "dot-2", "animate__animated", "animate__bounce");
+
+        const dot3 = document.createElement("div");
+        dot3.classList.add("dot", "dot-3", "animate__animated", "animate__bounce");
+
+        this.messagesBody.appendChild(typingDiv);
+        typingDiv.appendChild(dadImg);
+        typingDiv.appendChild(typingInner);
+        typingInner.appendChild(dotsDiv);
+        dotsDiv.appendChild(dot1);
+        dotsDiv.appendChild(dot2);
+        dotsDiv.appendChild(dot3);
+    }
+
+    newDadName() {
+        const dadDiv = document.createElement("div");
+        const dadInnerText = document.createElement("p");
+        dadInnerText.classList.add("small", "mb-1")
+        dadInnerText.textContent = "Dad";
+
+        dadDiv.appendChild(dadInnerText);
+        return dadDiv;
+    }
+
+    newDadImage() {
+        var inputInnerImg = document.createElement("img");
+        inputInnerImg.src = "assets/dadsgpt-icon.webp";
+        inputInnerImg.style = "width: 45px; height: 100%;";
+        return inputInnerImg;
     }
 }
 
-// document.getElementById('send-input').addEventListener('keypress', function(event) {
-//     if (event.key === 'Enter') {
-//         sendMessage();
-//     }
-// });
-// document.getElementById('send-button').addEventListener('click', function(event) {
-//     sendMessage();
-// });
+document.addEventListener("DOMContentLoaded", function() {
+    var messagesBody = document.getElementById("messages-body")
+    var messageFactory = new MessageFactory(messagesBody);
 
-
-// function startDotAnimation() {
-//     console.log("startDotAnimation");
-//     const dots = window.document.getElementsByClassName("dot");
-//     for (let i = 0; i < dots.length; i++) {
-//         const dot = dots[i];
-//         dot.offsetWidth;
-//         dot.classList.add('animate__animated', 'animate__bounce');
-//     };
-// }
-// function stopDotAnimation() {
-//     console.log("stopDotAnimation");
-//     const dots = window.document.getElementsByClassName("dot");
-//     for (let i = 0; i < dots.length; i++) {
-//         const dot = dots[i];
-//         dot.offsetWidth;
-//         dot.classList.remove('animate__animated', 'animate__bounce');
-//     };
-// }
-
-// const animateLoopMS = 5000;
-
-// // Inital animation
-// setTimeout(() => {
-//     startDotAnimation();
-//     setTimeout(() => {
-//         stopDotAnimation();
-//     }, (animateLoopMS - 100))
-// }, 10);
-
-// // Loop animation
-// setInterval(() => {
-//     startDotAnimation();
-//     setTimeout(() => {
-//         stopDotAnimation();
-//     }, (animateLoopMS - 100))
-// }, animateLoopMS);
+    window.messageFactory = messageFactory;
+});
